e4409824a1d55bd0ab48e96652d54f843584911c
searlejk <searlejk2@gmail.com>
2026-02-12 14:32:59 -0700

    ADDED login.jsx still need to fix that styling, it is too stretchy

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export function Login() {
-  return <div className="body bg-dark text-light"> Login </div>;
+  return (
+    <div className="body bg-dark text-light">
+      <main>
+        <form method="get" action="daily.html">
+          <h1>Log In</h1>
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Log In
+          </button>
+          <span className="text-muted">
+            Need an account?{" "}
+            <a href="create_account.html">Create Account</a>{" "}
+          </span>
+        </form>
+      </main>
+    </div>
+  );
 }
