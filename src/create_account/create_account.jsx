7636c424828391b5087a5f719da30da77f569254
searlejk <searlejk2@gmail.com>
2026-02-12 14:46:26 -0700

    Fixed navlink for create_account and login so that they work. I also did the jsx for create_account

--- src/create_account/create_account.jsx
+++ src/create_account/create_account.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function CreateAccount() {
-  return <div className="body bg-dark text-light"> Create Account </div>;
+  return (
+    <main>
+      <form method="get" action="daily.html">
+        <h1>Create Account</h1>
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingVerifyPassword"
+            placeholder="Verify Password"
+          />
+          <label for="floatingVerifyPassword">Verify Password</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Create Account
+        </button>
+        <span className="text-muted">
+          Need an account? <NavLink to="/">Log In</NavLink>{" "}
+        </span>
+      </form>
+    </main>
+  );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,38 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function Login() {
   return (
-    <div className="body bg-dark text-light">
-      <main>
-        <form method="get" action="daily.html">
-          <h1>Log In</h1>
-          <div className="form-floating">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Log In
-          </button>
-          <span className="text-muted">
-            Need an account?{" "}
-            <a href="create_account.html">Create Account</a>{" "}
-          </span>
-        </form>
-      </main>
-    </div>
+    <main>
+      <form method="get" action="daily.html">
+        <h1>Log In</h1>
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Log In
+        </button>
+        <span className="text-muted">
+          Need an account?{" "}
+          <NavLink to="create_account">Create Account</NavLink>{" "}
+        </span>
+      </form>
+    </main>
   );
 }
