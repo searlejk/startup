b7870340b82a32faf1c7717e53924ea6c0a00219
searlejk <searlejk2@gmail.com>
2026-02-12 14:10:48 -0700

    Got my individual jsx files working and also added the unlimited jsx which I forgot to add

--- src/app.jsx
+++ src/app.jsx
@@ -2,52 +2,64 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
-// import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
-// import { Login } from "./login/login";
-// import { Play } from "./play/play";
-// import { Scores } from "./scores/scores";
-// import { About } from "./about/about";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Daily } from "./daily/daily";
+import { CreateAccount } from "./create_account/create_account";
+import { Leaderboard } from "./leaderboard/leaderboard";
+import { Unlimited } from "./unlimited/unlimited";
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <h3>Flagle</h3>
-            </li>
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <h3>Flagle</h3>
+              </li>
 
-            <li className="curr_page">
-              <a href="login.html">Home</a>
-            </li>
-            <li>
-              <a href="daily.html">Daily</a>
-            </li>
-            <li>
-              <a href="unlimited.html">Unlimited</a>
-            </li>
-            <li>
-              <a href="leaderboard.html">Leaderboard</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+              <li className="curr_page">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="daily">Daily</NavLink>
+              </li>
+              <li>
+                <NavLink to="unlimited">Unlimited</NavLink>
+              </li>
+              <li>
+                <NavLink to="leaderboard">Leaderboard</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-      <main> Main Body Section</main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/daily" element={<Daily />} />
+          <Route path="/unlimited" element={<Unlimited />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/create_account" element={<CreateAccount />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <a href="https://github.com/searlejk/startup">Author: Jeremy Searle</a>
-      </footer>
-    </div>
+        <footer>
+          <a href="https://github.com/searlejk/startup">
+            Author: Jeremy Searle
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
-// function NotFound() {
-//   return (
-//     <div className="container">
-//       <h1>404 Not Found</h1>
-//       <p>The page you are looking for does not exist.</p>
-//     </div>
-//   );
-// }
+function NotFound() {
+  return (
+    <div className="container">
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
---
+++ src/unlimited/unlimited.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Unlimited() {
+  return <div className="body bg-dark text-light"> Unlimited </div>;
+}
