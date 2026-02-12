26648f2bd9f9286d7e5531c037b16407b4f92848
searlejk <searlejk2@gmail.com>
2026-02-12 14:35:05 -0700

    UPDATED leaderboard.jsx and still need to fix that styling

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,5 +1,112 @@
 import React from "react";
 
 export function Leaderboard() {
-  return <div className="body bg-dark text-light"> Leaderboard </div>;
+  return (
+    <div className="body bg-dark text-light">
+      <main>
+        <div className="flag_banner">
+          <img src="flag_banner.png" alt="flag banner image" />
+        </div>
+
+        <h1>Community: 9,347 games played</h1>
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Country</th>
+              <th scope="col">Daily Streak</th>
+              <th scope="col">Games Played</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Mark</td>
+              <td>USA</td>
+              <td>250</td>
+              <td>435</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>{" "}
+    </div>
+  );
 }
