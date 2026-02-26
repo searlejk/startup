e20468dc94827cabc7e56cffa79c531ced943f52
searlejk <searlejk2@gmail.com>
2026-02-25 20:38:28 -0700

    ADDING more to my flagle game logic

--- src/daily/flagleGame.jsx
+++ src/daily/flagleGame.jsx
@@ -21,6 +21,7 @@ export function flagleGame(props) {
       stripes: ["red", "white", "red"],
     },
   };
+  const guessCount = 0;
 
   async function onPressed(input) {
     if (allowPlayer) {
@@ -37,17 +38,64 @@ export function flagleGame(props) {
     }
   }
 
+  function checkGuess(guess) {
+    guessFlag = countries[guess];
+    trueFlag = countries[secretFlag];
+    const output = [];
+
+    for (i in Range(2)) {
+      if (guessFlag[i] === trueFlag[i]) {
+        output.concat(guessFlag[i]);
+      } else {
+        output.concat("grey");
+      }
+    }
+
+    return output;
+  }
+
+  function drawFlag(colors) {
+    return (
+      <svg viewBox="0 0 300 200" width="150" height="100">
+        <rect x="0" y="0" width="100" height="200" fill={colors[0]} />
+        <rect x="100" y="0" width="100" height="200" fill={colors[1]} />
+        <rect x="200" y="0" width="100" height="200" fill={colors[2]} />
+      </svg>
+    );
+  }
+
+  function returnRow(guess) {
+    feedbackFlag = drawFlag(checkGuess(guess));
+    guessFlag = drawFlag(countries[guess].stripes);
+
+    return (
+      <>
+        <div className="col">
+          Guess #{guessCount}
+          <br />({guess})
+          <br />
+          Correct!
+        </div>
+        <div className="col">{guessFlag}</div>
+        <div className="col">{feedbackFlag}</div>
+      </>
+    );
+  }
+
   async function newGame() {
     setAllowPlayer(false);
     await delay(1000);
+
+    secretFlag = getRandomFlag();
+    setAllowPlayer(true);
 
     // Let other players know a new game has started
     GameNotifier.broadcastEvent(userName, GameEvent.Start, {});
   }
 
   function getRandomFlag() {
-    let b = Array.from(countries.values());
-    return countries[Math.floor(Math.random() * countries.length)];
+    let b = Array.from(countries.names());
+    return countries[b[Math.floor(Math.random() * b.length)]].name;
   }
 
   function updateScoresLocal(newScore) {
