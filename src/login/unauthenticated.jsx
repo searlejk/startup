89058b06712e003dcd29bd9d5e250eed60b958b7
searlejk <searlejk2@gmail.com>
2026-03-12 12:29:54 -0600

    COPIED simon code over for login and also the viteconfig for reference and a starting point of my own code

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -11,13 +11,32 @@ export function Unauthenticated(props) {
   const navigate = useNavigate();
 
   async function loginUser() {
-    localStorage.setItem("userName", userName);
-    props.onLogin(userName);
+    loginOrCreate("/api/auth/login");
+    // localStorage.setItem("userName", userName);
+    // props.onLogin(userName);
   }
 
   async function createUser() {
-    localStorage.setItem("userName", userName);
-    props.onLogin(userName);
+    loginOrCreate("/api/auth/create");
+    // localStorage.setItem("userName", userName);
+    // props.onLogin(userName);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: "post",
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem("userName", userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
   }
 
   return (
