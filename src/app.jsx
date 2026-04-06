6dbd6e5f440d457971c1760db4967fae4db25c53
searlejk <searlejk2@gmail.com>
2026-04-06 11:36:38 -0600

    Deployed simon to the server

--- src/app.jsx
+++ src/app.jsx
@@ -119,7 +119,7 @@ function NotFound() {
   return (
     <div className="container">
       <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking for is not found.</p>
     </div>
   );
 }
